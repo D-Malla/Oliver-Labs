4c98bd7105cabe0c496ef0112a2abf006719b722
Dylan Malla <51140000+D-Malla@users.noreply.github.com>
2023-02-08 01:16:45 -0800

    Going back to normal. Need to reupload level 1

--- Source/Oliver_Labs/Private/Animations/OliverAnimInstance.cpp
+++ Source/Oliver_Labs/Private/Animations/OliverAnimInstance.cpp
@@ -2,4 +2,38 @@
 
 
 #include "Animations/OliverAnimInstance.h"
+#include "Characters/Oliver.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
+UOliverAnimInstance::UOliverAnimInstance()
+{
+	// Initialize Properties
+	Speed = 0.f;
+	bIsCrouched = false;
+	bIsInAir = false;
+}
+
+void UOliverAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	PlayerReference = Cast<AOliver>(TryGetPawnOwner());
+}
+
+void UOliverAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (PlayerReference)
+	{
+		UpdateAnimProperties();
+	}
+}
+
+void UOliverAnimInstance::UpdateAnimProperties()
+{
+	Speed = PlayerReference->GetVelocity().Size();
+	bIsCrouched = PlayerReference->GetIsCrouched();
+	bIsInAir = PlayerReference->GetMovementComponent()->IsFalling();
+}
